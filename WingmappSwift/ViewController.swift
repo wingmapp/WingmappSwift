3149c785b32e8fdb76c1a905d7865f59d95369ee
Thea Pappas <Wingmapp@Theas-MacBook-Pro.local>
2014-10-07 22:12:02 -0500

    Initial Commit

---
+++ WingmappSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WingmappSwift
+//
+//  Created by Thea Pappas on 10/7/14.
+//  Copyright (c) 2014 ___Wingmapp___. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
