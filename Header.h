e1420e1af22e9585edb8afa295ee5d45cf10b4fb
wingmapp <tpappas@wingmapp.com>
2015-06-23 02:00:33 -0500

    Swift
    Swift

---
+++ Header.h
@@ -0,0 +1,13 @@
+//
+//  Header.h
+//  WingmappSwift
+//
+//  Created by Thea Pappas on 12/9/14.
+//  Copyright (c) 2014 ___Wingmapp___. All rights reserved.
+//
+
+#ifndef WingmappSwift_Header_h
+#define WingmappSwift_Header_h
+
+
+#endif
